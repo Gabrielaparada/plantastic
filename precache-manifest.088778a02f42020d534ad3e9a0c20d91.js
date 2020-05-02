d98800358eafc2cf5b0aa49b9f547baadf7f7467
Gabriela Parada <gabrielaparada@Gabrielas-MacBook-Pro.local>
2020-05-01 22:23:19 -0400

    Updates

---
+++ precache-manifest.088778a02f42020d534ad3e9a0c20d91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadc510742054cc379a350af81adcfba",
+    "url": "/gabriela-parada-project-five/index.html"
+  },
+  {
+    "revision": "11a881125ed8b8f51283",
+    "url": "/gabriela-parada-project-five/static/css/main.307d54d9.chunk.css"
+  },
+  {
+    "revision": "7ecc1ee69a4f2cf92cee",
+    "url": "/gabriela-parada-project-five/static/js/2.df3b2027.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/gabriela-parada-project-five/static/js/2.df3b2027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a881125ed8b8f51283",
+    "url": "/gabriela-parada-project-five/static/js/main.fd9dd282.chunk.js"
+  },
+  {
+    "revision": "b400e211fb16ad63d605",
+    "url": "/gabriela-parada-project-five/static/js/runtime-main.8911b858.js"
+  },
+  {
+    "revision": "6cacd33a526da1e38d68c97285d35c69",
+    "url": "/gabriela-parada-project-five/static/media/photo.6cacd33a.png"
+  },
+  {
+    "revision": "382a6da554577cceaeb4b183284aebbc",
+    "url": "/gabriela-parada-project-five/static/media/plantsHeaderedit1.382a6da5.jpg"
+  }
+]);
